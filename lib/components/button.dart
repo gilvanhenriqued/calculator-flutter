85ed58e1ca41684981c8ca89b194b336cc0a63b3
Gilvan Henrique <gilvanh.15@gmail.com>
2020-06-20 19:55:24 -0300

    changing button colors

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -2,18 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Button extends StatelessWidget {
+  static const DARK = Color.fromRGBO(82,82,82,1);
+  static const DEFAULT = Color.fromRGBO(112,112,112,1);
+  static const OPERATION = Color.fromRGBO(250,158,13,1);
+
   final String text;
+  final bool bigButton;
+  final Color color; 
 
   Button({
     @required this.text,
+    this.bigButton = false,
+    this.color = DEFAULT,
+  });
+
+  Button.big({
+    @required this.text,
+    this.bigButton = true,
+    this.color = DEFAULT,
+  });
+
+  Button.operation({
+    @required this.text,
+    this.bigButton = false,
+    this.color = OPERATION,
   });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      flex: 1,
+      flex: bigButton ? 2 : 1,
       child: RaisedButton(
-        child: Text(text),
+        color: this.color,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.w200
+          )
+        ),
         onPressed: () {},
       )
     );
